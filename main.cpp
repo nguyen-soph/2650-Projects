3f704639c238216bef141dd48ace66bc53fa901b
nguyen-soph <nguyen.sop@northeastern.edu>
2025-05-06 12:05:14 -0400

    Test with project 1

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//
+//  main.cpp
+//  
+//
+//  Created by Sophia Nguyen on 5/6/25.
+//
+
+#include "main.hpp"
---
+++ main.hpp
@@ -0,0 +1,13 @@
+//
+//  main.hpp
+//  
+//
+//  Created by Sophia Nguyen on 5/6/25.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
