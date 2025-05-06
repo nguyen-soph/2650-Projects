2719051e4176dcde003ff422e95b6c37eb0d607f
nguyen-soph <nguyen.sop@northeastern.edu>
2025-05-06 12:18:32 -0400

    Changing project to c++ command line

---
+++ 2560-Project-1/2560-Project-1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2560-Project-1
+//
+//  Created by Sophia Nguyen on 5/6/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
